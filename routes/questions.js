9fc5c6bb08fee690f5a4aff6259fea376e5aa4c8
Andre Machado <andreoliveiramachado8@gmail.com>
2022-04-30 15:43:40 +0200

    random button

--- routes/questions.js
+++ routes/questions.js
@@ -2,14 +2,20 @@ const express = require('express');
 const Question = require('../models/question');
 const router = express.Router();
 const routeGuard = require('./../middleware/route-guard');
+const helpers = require('handlebars-helpers')();
 
 router.get('/', routeGuard, (req, res, next) => {
   Question.find()
     .then((questions) => {
       const randomQuestion =
         questions[Math.floor(Math.random() * questions.length)];
+      const randomPosition = Math.random();
       console.log(randomQuestion);
-      res.render('questions', { randomQuestion });
+      console.log(randomPosition);
+      res.render('questions', {
+        randomQuestion,
+        randomPosition: randomPosition > 0.5
+      });
     })
     .catch((error) => next(error));
 });
