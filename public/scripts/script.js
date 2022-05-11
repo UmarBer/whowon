7b0235237b4decb0db4413c7c43bdae0714b7d5f
Andres Vazquez <andres.vazquez.martin@gmail.com>
2022-05-11 20:32:19 +0100

    added social media share buttons

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,18 @@
+//const highScore = require('../routes/scoreboard');
+
+const facebook = document.querySelector('.facebook');
+const whatsapp = document.querySelector('.whatsapp');
+const twitter = document.querySelector('.twitter');
+const telegram = document.querySelector('.telegram');
+
+const pageUrl = location.href;
+const message = 'this is a test';
+
+const facebookApi = `https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fguesswhowon.herokuapp.com%2F&amp;src=sdkpreparse`;
+const whatsappApi = `https://wa.me/?text=${pageUrl}. ${message}`;
+const twitterApi = `https://twitter.com/intent/tweet?text=${pageUrl}. ${message}`;
+const telegramApi = `https://t.me/share/url?url=${pageUrl}&text=${message}`;
+
 window.addEventListener(
   'load',
   () => {
@@ -5,3 +20,17 @@ window.addEventListener(
   },
   false
 );
+
+facebook.addEventListener('click', () => {
+  window.open((url = facebookApi), (target = 'blank'));
+});
+
+whatsapp.addEventListener('click', () => {
+  window.open((url = whatsappApi), (target = 'blank'));
+});
+twitter.addEventListener('click', () => {
+  window.open((url = twitterApi), (target = 'blank'));
+});
+telegram.addEventListener('click', () => {
+  window.open((url = telegramApi), (target = 'blank'));
+});
--- routes/scoreboard.js
+++ routes/scoreboard.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const routeGuard = require('./../middleware/route-guard');
 const User = require('../models/user');
 
+// const telegram = document.querySelector('.telegram');
+// const twitter = document.querySelector('.twitter');
+// const facebook = document.querySelector('.facebook');
+
 router.get('/', (req, res, next) => {
   User.find({})
     .limit(10)
